7a4a59e0ace9c2c04d2d77152f1137538e1b3eaa
Carlos Mexia <carlos.lia.hmo@gmail.com>
2015-05-15 10:45:07 -0700

    Push con la funcion incompleta del reporte para wizard. revision

---
+++ wizard/report/attend.py
@@ -0,0 +1,37 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (c) 2014 ZestyBeanz Technologies Pvt. Ltd.
+#    (http://wwww.zbeanztech.com)
+#    contact@zbeanztech.com
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+import time
+from openerp.report import report_sxw
+import mx.DateTime
+
+class attend_parser(report_sxw.rml_parse):
+    def __init__(self, cr, uid, name, context):
+        super(attend_parser, self).__init__(cr, uid, name, context=context)
+        self.localcontext.update({
+            'time': time,
+        })
+
+report_sxw.report_sxw('report.attend_report',
+                    'attend.supply.request',
+                    'addons/argil_supply_request/wizard/report/attend_report.rml',
+                    parser=attend_parser, header=False)
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
